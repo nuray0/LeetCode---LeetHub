7cae63a94ed222dc5ecf17a4c7289a90e719a28b
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-07-08 14:57:48 +0600

    Time: 35 ms (81.64%), Space: 13.8 MB (57.14%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,11 @@
+
+    
+class Solution:
+    def climbStairs(self, n):
+        a,b = 1,0
+        for _ in range(n):
+            a,b = a+b,a
+        return a
+                
+        
+        
