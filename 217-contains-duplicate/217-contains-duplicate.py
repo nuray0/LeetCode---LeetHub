ef30dddc6177ccb76570b83b9674b682a9864bcc
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-07-08 14:57:03 +0600

    Time: 547 ms (71.75%), Space: 26 MB (72.36%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,18 @@
+
+class Solution:
+
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        uniques = {}
+        for n in nums:
+            if n in uniques:
+                uniques[n] += 1
+            else:
+                uniques[n] = 1
+        
+        for key in uniques:
+            if uniques[key] > 1:
+                return True
+        return False
+   
+    
+ 
