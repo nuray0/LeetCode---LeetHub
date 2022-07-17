e6947f2ebcc49847823f2165a4d30b1a27fcad68
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-07-17 22:29:26 +0600

    Time: 71 ms (62.38%), Space: 13.9 MB (85.59%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -1,20 +1,33 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-
-     
-        
+    
+    
+    
         # dictionary to count
+        # almost the same runtime and memory usage as pointers
+#        output = []
+#        counts = {}
+#
+#        for num in nums1:
+#            counts[num] = counts.get(num, 0) + 1
+#
+#        for num in nums2:
+#            if num in counts and counts[num] > 0:
+#                output.append(num)
+#                counts[num] -= 1
+#
+#        return output
+
+
+        # same as dictionary, but using counter
         output = []
-        counts = {}
-
-        for num in nums1:
-            counts[num] = counts.get(num, 0) + 1
-
+        counts = collections.Counter(nums1)
+        
         for num in nums2:
             if num in counts and counts[num] > 0:
-                output.append(num)
+                output += num,
                 counts[num] -= 1
-
+                
         return output
 
     
