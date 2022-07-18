7695e2bec9b84a99dc18ab7d3edb49f91a67e19e
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-07-18 14:24:32 +0600

    Time: 111 ms (75.78%), Space: 14.8 MB (15.96%) - LeetHub

---
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -0,0 +1,29 @@
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        
+        m = len(mat)
+        n = len(mat[0])
+        
+        if m < 1 or m > 100 or n < 1 or n > 100:
+            raise ValueError("invalid value for either m or n")
+            
+        if r < 1 or r > 300 or c < 1 or c > 300:
+            raise ValueError("invalid value for either r or c")
+        
+        if m * n != r * c:
+            return mat   
+        elif m == r and n == c:
+            return mat
+        
+        output = [[]]
+        
+        for i in range(m):
+            for j in range(n):
+                if len(output[-1]) < c:
+                    output[-1].append(mat[i][j])
+                else:
+                    output.append([mat[i][j]])
+                    
+        return output
+                    
+        
