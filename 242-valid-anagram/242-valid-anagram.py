09e184fdbe47429985ebca18144961a29b9adcea
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-07-20 00:26:02 +0600

    Time: 46 ms (95.22%), Space: 15.1 MB (11.53%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,11 +1,5 @@
+
+    
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        for i in s:
-            if i not in t:
-                return False
-            else:
-                index_delete = t.index(i)
-                t = t[:index_delete] + t[index_delete + 1:]
-        if len(t) == 0:
-            return True
-        return False
+        return sorted(s) == sorted(t)
