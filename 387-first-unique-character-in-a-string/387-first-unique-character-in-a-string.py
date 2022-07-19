b46335a49122ed0a2dd6c095b306f11a3db1dbcf
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-07-20 00:00:50 +0600

    Time: 156 ms (59.44%), Space: 14.3 MB (17.64%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
@@ -4,8 +4,14 @@ class Solution:
                       
         count = collections.Counter(s)
         
-        for i in range(len(s)):
-            if count[s[i]] == 1:
-                return i
+#        for i in range(len(s)):
+#            if count[s[i]] == 1:
+#                return i
+            
+        for index, char in enumerate(s):
+            if count[char] == 1:
+                return index
                 
         return -1
+    
+    
