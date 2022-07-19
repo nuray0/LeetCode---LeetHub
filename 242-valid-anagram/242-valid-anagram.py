d4d9d8fd8b7d4acbca64716111f2edcda04ed800
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-07-20 00:27:17 +0600

    Time: 99 ms (20.75%), Space: 15.2 MB (11.53%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,5 +1,15 @@
-
-    
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
+
+    
+        # method for manual comparison
+#        for i in s:
+#            if i not in t:
+#                return False
+#            else:
+#                index_delete = t.index(i)
+#                t = t[:index_delete] + t[index_delete + 1:]
+#        if len(t) == 0:
+#            return True
+#        return False
