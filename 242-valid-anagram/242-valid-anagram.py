e4b9d6a9b443ed50ba79e0d0f484966cec159522
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-07-20 00:27:53 +0600

    Time: 93 ms (27.71%), Space: 15.2 MB (11.53%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -3,13 +3,3 @@ class Solution:
         return sorted(s) == sorted(t)
 
     
-        # method for manual comparison
-#        for i in s:
-#            if i not in t:
-#                return False
-#            else:
-#                index_delete = t.index(i)
-#                t = t[:index_delete] + t[index_delete + 1:]
-#        if len(t) == 0:
-#            return True
-#        return False
