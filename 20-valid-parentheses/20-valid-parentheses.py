6af2cdff7513f9e47993ebed43ada2ed29f0a12b
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-07-27 15:26:42 +0600

    Time: 51 ms (42.95%), Space: 13.8 MB (70.03%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,9 +1,19 @@
-
-    
 class Solution:
     def isValid(self, s: str) -> bool:
-        while len(s) > 0:
-            l = len(s)
-            s = s.replace('()','').replace('{}','').replace('[]','')
-            if l==len(s): return False
-        return True
+        stack = []
+        brackets = {'(':')', '{':'}', '[':']'}
+        
+        for i in s:
+            if i in brackets:
+                stack.append(i)
+            elif len(stack) == 0 or brackets[stack.pop()] != i:
+                return False
+            
+        return len(stack) == 0
+    
+        # replacing closed brackets with empty string
+#        while len(s) > 0:
+#            l = len(s)
+#            s = s.replace('()','').replace('{}','').replace('[]','')
+#            if l==len(s): return False
+#        return True
