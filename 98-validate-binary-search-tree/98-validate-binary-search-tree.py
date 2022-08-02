871cc3da102cd9630c72b2a594434ea700fed8bd
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-08-02 17:40:49 +0600

    Time: 70 ms (51.63%), Space: 17 MB (26.14%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -15,7 +15,8 @@ class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         out = []
         self.helper(root, out)
-        for i in range(1, len(out)):
+        length = len(out)
+        for i in range(1, length):
             if out[i - 1] >= out[i]:
                 return False
         return True
