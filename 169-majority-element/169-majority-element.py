47624c83df89698a7d23476c3e9a0df3f4b961c6
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-08-03 15:49:21 +0600

    Time: 257 ms (52.29%), Space: 15.5 MB (86.47%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,16 +1,9 @@
-
+import random
 
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        counts = {}
-        half = len(nums) // 2
-        if half == 0:
-            return nums[0]
-        
-        for num in nums:
-            if num not in counts:
-                counts[num] = 1
-            else:
-                counts[num] += 1
-                if counts[num] > half:
-                    return num
+    def majorityElement(self, nums: List[int]) -> int: 
+        max_count = len(nums) // 2
+        while True:
+            number = random.choice(nums)
+            if sum(1 for num in nums if num == number) > max_count:
+                return number
