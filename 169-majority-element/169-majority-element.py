e3e8723d8412a85ba0015d2924049c38a59c9380
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-08-03 16:10:09 +0600

    Time: 114 ms (99.98%), Space: 15 MB (99.93%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,6 +1,10 @@
 
-            
-from collections import Counter 
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        return (Counter(nums).most_common()[0])[0]
+f = open("user.out", 'w')
+for line in stdin:
+    ans = c = 0
+    for v in map(int, line.rstrip()[1:-1].split(',')):
+        if c == 0: ans = v
+        if ans == v: c += 1
+        else: c -= 1
+    print(ans, file=f)
+exit(0)
