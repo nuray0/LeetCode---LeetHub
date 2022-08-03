5395b34a8e808fde27deecb14c3861bc45b50438
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-08-03 15:41:10 +0600

    Time: 364 ms (8.38%), Space: 15.6 MB (35.08%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,4 +1,4 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:    
-        counts = collections.Counter(nums)
-        return max(counts.keys(), key = counts.get)
+    def majorityElement(self, nums: List[int]) -> int: 
+        nums.sort()
+        return nums[len(nums) // 2]
