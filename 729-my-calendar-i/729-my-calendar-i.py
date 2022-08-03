2782a7f07c90febca2bceff265f6ffe34e3a9e7e
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-08-03 14:11:22 +0600

    Time: 222 ms (97.05%), Space: 14.7 MB (92.29%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -1,13 +1,27 @@
 class MyCalendar:
 
+    
     def __init__(self):
-        self.calendar = []
+        self.intervals = []
 
-    def book(self, start: int, end: int) -> bool:
-        for s, e in self.calendar:
-            if s < end and e > start:
-                return False
-        self.calendar.append((start, end))
+    def book(self, start, end):
+        """
+        :type start: int
+        :type end: int
+        :rtype: bool
+        """
+        if end <= start:
+            return False
+        
+        i = bisect.bisect_right(self.intervals, start)
+        if i % 2:            # start is in some stored interval
+            return False
+        
+        j = bisect.bisect_left(self.intervals, end)
+        if i != j:
+            return False
+        
+        self.intervals[i:i] = [start, end]
         return True
         
         
