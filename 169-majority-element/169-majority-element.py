1270b5c90d4c54660cc29235fab1125142129a6a
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-08-03 15:57:41 +0600

    Time: 302 ms (29.45%), Space: 15.6 MB (35.08%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,9 +1,11 @@
-import random
-
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int: 
-        max_count = len(nums) // 2
-        while True:
-            number = random.choice(nums)
-            if sum(1 for num in nums if num == number) > max_count:
-                return number
+    def majorityElement(self, nums):
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
