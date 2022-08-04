c7b22ab87d44fbea1bcb83d7ce0b52568fa989b0
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-08-04 12:58:50 +0600

    Time: 46 ms (67.88%), Space: 13.8 MB (97.45%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -8,15 +8,11 @@ class Solution:
         i = 0
         while i <= before_2:
             if nums[i] == 0:
-                temp = nums[after_0]
-                nums[after_0] = nums[i]
-                nums[i] = temp
+                nums[after_0], nums[i] = nums[i], nums[after_0]
                 after_0 += 1
                 i += 1
             elif nums[i] == 2:
-                temp = nums[i]
-                nums[i] = nums[before_2]
-                nums[before_2] = temp
+                nums[before_2], nums[i] = nums[i], nums[before_2]
                 before_2 -= 1
             else:
                 i += 1
