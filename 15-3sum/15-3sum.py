67636e58015ef62529324b6fd10b9bce6fa25c03
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-08-04 11:12:13 +0600

    Time: 1328 ms (52.07%), Space: 18.5 MB (13.97%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,20 +1,50 @@
 class Solution:
+
+
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        length = len(nums)
-        if length < 3:
-            return []
-        out = {}
-        
-        d = {}
+
+        res = set()
+
+        #1. Split nums into three lists: negative numbers, positive numbers, and zeros
+        n, p, z = [], [], []
+        for num in nums:
+            if num > 0:
+                p.append(num)
+            elif num < 0: 
+                n.append(num)
+            else:
+                z.append(num)
+
+        #2. Create a separate set for negatives and positives for O(1) look-up times
+        N, P = set(n), set(p)
+
+        #3. If there is at least 1 zero in the list, add all cases where -num exists in N and num exists in P
+        #   i.e. (-3, 0, 3) = 0
+        if z:
+            for num in P:
+                if -1*num in N:
+                    res.add((-1*num, 0, num))
+
+        #3. If there are at least 3 zeros in the list then also include (0, 0, 0) = 0
+        if len(z) >= 3:
+            res.add((0,0,0))
+
+        #4. For all pairs of negative numbers (-3, -1), check to see if their complement (4)
+        #   exists in the positive number set
+        length = len(n)
         for i in range(length):
-            d[nums[i]] = i
-    
+            for j in range(i+1,length):
+                target = -1*(n[i]+n[j])
+                if target in P:
+                    res.add(tuple(sorted([n[i],n[j],target])))
+
+        #5. For all pairs of positive numbers (1, 1), check to see if their complement (-2)
+        #   exists in the negative number set
+        length = len(p)
         for i in range(length):
-            for j in range(i + 1, length):
-                target = - nums[i] - nums[j]
-                if target in d and d[target] != i and d[target] != j:
-                    to_append = tuple(sorted((nums[i], nums[j], target)))
-                    if not to_append in out:
-                        out[to_append] = 0
-                    
-        return out
+            for j in range(i+1,length):
+                target = -1*(p[i]+p[j])
+                if target in N:
+                    res.add(tuple(sorted([p[i],p[j],target])))
+
+        return res
