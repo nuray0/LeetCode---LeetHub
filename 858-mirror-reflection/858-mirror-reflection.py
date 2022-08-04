79147c6e78c9ee6a376319e5f0cd2f78313f028e
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-08-04 21:37:33 +0600

    Time: 35 ms (90.10%), Space: 14 MB (16.83%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.py
+++ 858-mirror-reflection/858-mirror-reflection.py
@@ -2,5 +2,13 @@ class Solution:
     def mirrorReflection(self, p: int, q: int) -> int:
         while p % 2 == 0 and q % 2 == 0:
             p, q = p / 2, q / 2
-        return int(1 - p % 2 + q % 2)
+            
+        if p % 2==0 and q % 2==1: #if p even and q odd
+            return 2
+        
+        elif p % 2==1 and q % 2==0: #if p odd and q even
+            return 0
+        
+        else:                   #if p odd and q odd
+            return 1
         
