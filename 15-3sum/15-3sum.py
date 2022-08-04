65dfb136b16872338130831447e02ffe824a4ec2
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-08-04 11:16:34 +0600

    Time: 191 ms (99.99%), Space: 18.3 MB (22.07%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,50 +1,36 @@
 class Solution:
-
-
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) < 3:
+            return []
 
-        res = set()
+        counter = {}
+        for i in nums:
+            if i not in counter:
+                counter[i] = 0    
+            counter[i] += 1
 
-        #1. Split nums into three lists: negative numbers, positive numbers, and zeros
-        n, p, z = [], [], []
-        for num in nums:
-            if num > 0:
-                p.append(num)
-            elif num < 0: 
-                n.append(num)
-            else:
-                z.append(num)
+        nums = sorted(counter)
+        if nums[0] > 0 or nums[-1] < 0:
+            return []
+        
+        output = []
+        # find answer with no duplicates within combo
+        for i in range(len(nums)-1):
+            # search range
+            twoSum = -nums[i]
+            min_half, max_half = twoSum - nums[-1], twoSum / 2
+            l = bisect_left(nums, min_half, i + 1)
+            r = bisect_left(nums, max_half, l)
+            
+            for j in nums[l:r]:
+                if twoSum - j in counter:
+                    output.append([nums[i], j, twoSum - j])
 
-        #2. Create a separate set for negatives and positives for O(1) look-up times
-        N, P = set(n), set(p)
-
-        #3. If there is at least 1 zero in the list, add all cases where -num exists in N and num exists in P
-        #   i.e. (-3, 0, 3) = 0
-        if z:
-            for num in P:
-                if -1*num in N:
-                    res.add((-1*num, 0, num))
-
-        #3. If there are at least 3 zeros in the list then also include (0, 0, 0) = 0
-        if len(z) >= 3:
-            res.add((0,0,0))
-
-        #4. For all pairs of negative numbers (-3, -1), check to see if their complement (4)
-        #   exists in the positive number set
-        length = len(n)
-        for i in range(length):
-            for j in range(i+1,length):
-                target = -1*(n[i]+n[j])
-                if target in P:
-                    res.add(tuple(sorted([n[i],n[j],target])))
-
-        #5. For all pairs of positive numbers (1, 1), check to see if their complement (-2)
-        #   exists in the negative number set
-        length = len(p)
-        for i in range(length):
-            for j in range(i+1,length):
-                target = -1*(p[i]+p[j])
-                if target in N:
-                    res.add(tuple(sorted([p[i],p[j],target])))
-
-        return res
+        # find ans with duplicates within combo
+        for k in counter:
+            if counter[k] > 1:
+                if k == 0 and counter[k] >= 3:
+                    output.append([0, 0, 0])
+                elif k != 0 and -2 * k in counter:
+                    output.append([k, k, -2 * k])
+        return output
