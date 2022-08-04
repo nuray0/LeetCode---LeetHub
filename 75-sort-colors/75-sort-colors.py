07ef8de44c7b0c330b7642f0fa816e7d39df393b
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-08-04 12:49:03 +0600

    Time: 66 ms (19.39%), Space: 14 MB (15.32%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -2,8 +2,21 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
-        """           
-     
-        
-        nums.sort()
+        """               
+        mid = 1       # pivot
+        i = 0         # less than mid
+        j = 0         # current
+        k = len(nums) # greater than mid
+
+        while j < k:
+            cur = nums[j]
+            if cur > mid:
+                k -= 1
+                nums[j], nums[k] = nums[k], nums[j]
+            elif cur < mid:
+                nums[j], nums[i] = nums[i], nums[j]
+                j += 1
+                i += 1
+            else:
+                j += 1
             
