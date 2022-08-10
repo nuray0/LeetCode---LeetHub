db4e1e17218fd5d92f5ae2da7293740ae1f9ed69
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-08-10 17:16:13 +0600

    Time: 48 ms (52.86%), Space: 13.8 MB (62.27%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -0,0 +1,15 @@
+class Solution():
+    def getRow(self, r):
+        ans = [1]*(r+1);
+        up = r
+        down = 1
+        for i in range(1, r):
+            ans[i] = int(ans[i-1]*up/down);
+            up = up - 1
+            down = down + 1
+        return ans;
+
+                
+            
+            
+        
