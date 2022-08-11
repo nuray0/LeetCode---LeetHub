2a2920189f9e2255e16cd0df5d87098300d98115
nuray0 <83400916+nuray0@users.noreply.github.com>
2022-08-11 19:36:57 +0600

    Time: 47 ms (60.39%), Space: 13.9 MB (35.89%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -0,0 +1,15 @@
+
+                
+class Solution:
+    def generateMatrix(self, n): 
+        """ 
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        res, lo = [[n*n]], n*n 
+        while lo > 1:
+            lo, hi = lo - len(res), lo
+            #print('res:', res)
+            res = [[i for i in range(lo, hi)]] + [list(j) for j in zip(*res[::-1])]
+        return res 
+        
